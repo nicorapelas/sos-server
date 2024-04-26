fa3ddd89ab4d63d521e20b9f0499079d38f5311f
nicorapelas <nicorapelas@gmail.com>
2024-04-26 16:56:39 +0200

    updates to webSocket middleware

--- middlewares/webSocket.js
+++ middlewares/webSocket.js
@@ -2,31 +2,33 @@ const WebSocket = require('ws')
 
 function setupWebSocket(server) {
   const wss = new WebSocket.Server({ server })
-
   wss.on('connection', function connection(ws) {
     console.log('A new client connected.')
-
     ws.on('message', function incoming(data) {
+      console.log('Received message:', data)
       try {
         const messageObject = JSON.parse(data) // Parsing the JSON string
-        console.log('received:', messageObject.message)
-
-        // Broadcast incoming message to all clients
-        wss.clients.forEach(function each(client) {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(messageObject.message) // Send the actual message part
-          }
-        })
+        if (messageObject.panic) {
+          console.log('Broadcasting panic data:', messageObject)
+          // Broadcast panic message to all clients
+          wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify(messageObject)) // Send the entire panicData object
+            }
+          })
+        }
       } catch (error) {
         console.error('Failed to parse message:', error)
       }
     })
-
     ws.on('close', () => {
       console.log('Client has disconnected.')
     })
-
+    ws.on('error', (error) => {
+      console.error('WebSocket error:', error)
+    })
     ws.send(JSON.stringify({ message: 'Welcome to the chat!' })) // Send initial message as JSON
+    console.log('Welcome message sent.')
   })
 }
 
